b495c0f61a896987bbdbde6513ba60782dedc1c6
kobayashi-tbn <kobayashi.tbn@gmail.com>
2013-04-13 02:39:34 +0900

    add extention white list

---
+++ lib/watarase/extension_whitelist.rb
@@ -0,0 +1,20 @@
+module Watarase
+  module ImageHolder
+    module ExtensionWhitelist
+
+      def self.included(model)
+        model.send(:before_save, :check_extension)
+      end
+
+      def check_extension
+        if filename && extension_white_list && !extension_white_list.include?(File.extname(filename).sub(/\./, ''))
+          raise StandardError, "Unsupported file " + filename
+        end
+      end
+
+      def extension_white_list
+        %w(jpg jpeg gif png)
+      end
+    end
+  end
+end
